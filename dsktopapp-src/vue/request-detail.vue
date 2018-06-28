5d3b22706dbb221b77a88a2e7cee4dd508b25d45
yoshiyuki.mizogami <mizogami00@gmail.com>
2018-06-29 00:55:37 +0900

    layout fix

--- dsktopapp-src/vue/request-detail.vue
+++ dsktopapp-src/vue/request-detail.vue
@@ -1,80 +1,159 @@
+<style>
+.cover{
+  width:100%;
+  height:100%;
+  position:fixed;
+  top:0;
+  left:0;
+  z-index:1000;
+  background-color:rgb(0,0,0,.5);
+}
+.request-detail{
+  width:1000px;
+  height:80%;
+  background-color:white;
+  border-radius:5px;
+  padding:5px;
+  z-index:100000;
+  margin:auto;
+}
+.request-header{
+  height:300px;
+}
+.chat-body{
+  height:calc(100% - 400px);
+}
+.chat-console{
+  height:80px;
+}
+/* タイムライン部分③ */
+.chat-body{
+  overflow-y: scroll;
+  width: 100%;
+  border-right: 1px solid #ddd;
+  border-left: 1px solid #ddd;
+  background-color: #eee;
+  box-shadow: 0px 2px 2px 0px rgba(0,0,0,0.2) inset;/*ヘッダーの下に影を入れる*/
+}
+  /* メッセージ全般のスタイル */
+.message {
+    margin: 0px;
+    padding: 0 14px;/*吹き出しがタイムラインの側面にひっつかない様に隙間を開ける*/
+    font-size: 16px;
+    word-wrap: break-word;/* 吹き出し内で自動で改行 */
+    white-space: normal;/*指定widthに合わせて、文字を自動的に改行*/
+}
+/* メッセージ１（左側） */
+.left {
+  float: left;/*吹き出しをmessagesに対して左寄せ*/
+  line-height: 1.3em;
+}
+/* メッセージ２（右側） */
+.right {
+  float: right;/*吹き出しをmessagesに対して右寄せ*/
+  line-height: 1.3em;
+}
+/* 回り込みを解除 */
+.clear {
+    clear: both; /* 左メッセージと右メッセージの回り込み(float)の効果の干渉を防ぐために必要（これが無いと、自分より下のメッセージにfloatが影響する） */
 
+}
+
+/* テキストエリア、送信ボタン④ */
+.message-send-btn{
+  background-color:#eee;/*タイムラインの色と同じにする*/
+  border: 1px solid #ddd;
+  height: 100%;
+  vertical-align: top;
+  padding: 4px;
+}
+.chat-input{
+  width:80%;
+  height:100%;
+  resize:none;
+  border-radius:5px;
+  padding:5px;
+  font-size:20px;
+}
+
+/*
+*追加
+*/
+.message_content{
+  padding: 20px;/*文字や画像（コンテンツ）の外側に隙間を入れる*/
+}
+
+/* メッセージ１（左側） */
+.left {
+  float: left;/*吹き出しをmessagesに対して左寄せ*/
+  line-height: 1.3em;
+}
+/*
+*追加
+*/
+.left .message_box {
+  color: #333;/*テキストを黒にする*/
+  background: #fff;
+  border: 2px solid #13178E;
+  border-radius: 30px 30px 30px 0px;/*左下だけ尖らせて吹き出し感を出す*/
+  margin-right: 50px;/*左側の発言だとわかる様に、吹き出し右側に隙間を入れる*/
+}
+
+/* メッセージ２（右側） */
+.right {
+float: right;/*吹き出しをmessagesに対して右寄せ*/
+line-height: 1.3em;
+}
+/*
+*追加
+*/
+.right .message_box {
+    color: #fff;/*テキストを白にする*/
+    background: #13178E;
+    border: 2px solid #13178E;
+    border-radius: 30px 30px 0px 30px;/*右下だけ尖らせて吹き出し感を出す*/
+    margin-left: 50px;/*右側の発言だとわかる様に、吹き出し左側に隙間を入れる*/
+}
+</style>
 <template>
-  <div class="cover">
-    <div class="request-detail">
-      <div class="header">
-        <div class="title">
-          腕時計の見積依頼
-       </div>
+<div class="cover">
+  <div class="request-detail">
+    <div class="request-header">
+      <div class="title">
+        腕時計の見積依頼
       </div>
-    <div class="categories">
-      カテゴリー：腕時計
+      <div class="comment">
+        説明
+        ガラス割れました。
+        見積もりお願いいたします。
+      </div>
     </div>
-     <div class="comment">
-       説明
-       ガラス割れました。
-       見積もりお願いいたします。
-     </div>
-<div class="top-img">
-</div>
-     <div class="timeline">
-<body>
-    <div id="your_container">
-
-        <!-- チャットの外側部分① -->
-        <div id="bms_messages_container">
-            <!-- ヘッダー部分② -->
-            <div id="bms_chat_header">
-                <!--ステータス-->
-                <div id="bms_chat_user_status">
-                    <!--ステータスアイコン-->
-                    <div id="bms_status_icon">✉</div>
-                    <!--ユーザー名-->
-                    <div id="chat-title">メッセージ</div>
-                </div>
-            </div>
-
-            <!-- タイムライン部分③ -->
-            <div id="bms_messages">
-
-                <!--メッセージ１（左側）-->
-                <div class="bms_message bms_left">
-                    <div class="bms_message_box">
-                        <div class="bms_message_content">
-                            <div class="bms_message_text">ほうほうこりゃー便利じゃないか</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="bms_clear"></div><!-- 回り込みを解除（スタイルはcssで充てる） -->
-
-                <!--メッセージ２（右側）-->
-                <div class="bms_message bms_right">
-                    <div class="bms_message_box">
-                        <div class="bms_message_content">
-                            <div class="bms_message_text">うん、まあまあいけとるな</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="bms_clear"></div><!-- 回り込みを解除（スタイルはcssで充てる） -->
-            </div>
-
-            <!-- テキストボックス、送信ボタン④ -->
-            <div id="bms_send">
-                     <input type="hidden" name="item_id" value="m96627376588">
-          <textarea type="text" name="body" value="" class="textarea" cols="60" rows="8"></textarea>
-          <button type="submit" class="btn" >
-          <i class="icon-message"></i>
-          <span>取引メッセージを送る</span>
-          </button>
-            </div>
+    <div class="chat-body">
+      <div class="message left">
+        <div class="message_box">
+          <div class="message_content">
+            <div class="message_text">ほうほうこりゃー便利じゃないか</div>
+          </div>
         </div>
+      </div>
+      <div class="clear"></div>
+      <div class="message right">
+        <div class="message_box">
+          <div class="message_content">
+            <div class="message_text">うん、まあまあいけとるな</div>
+          </div>
+        </div>
+      </div>
+      <div class="clear"></div>
     </div>
-</body>
-
-    </div>  
- 
+    <div class="chat-console">
+      <textarea class="chat-input"></textarea>
+      <button class="message-send-btn">
+        取引メッセージを送る
+      </button>
     </div>
   </div>
+</div>
 </template>
 <script>
 export default {
--- dsktopapp-src/vue/root.vue
+++ dsktopapp-src/vue/root.vue
@@ -1,3 +1,8 @@
+<style>
+input, textarea,select{
+  font-family:inherit;
+}
+</style>
 <template>
   <div id="app">
     <h1 class="header"><img class="header-icon" src="img/logo.png"/>
@@ -24,17 +29,17 @@
           <div v-if="!editDesc" class="store-info">{{store.description}}</div>
           <div v-else class="store-info"><textarea v-model="store.description"></textarea></div>
         </div>
-        <div class="rate-avg">総合評価:<rate-parts :rate="store.rateAverage"></rate-parts></div>
+        <div class="rate-avg">総合評価:<rate-parts :rate="store.rateAverage"/></div>
 
         <div>最新のコメント</div>
         <div class="reputations">
           <div class="reputation" v-for="(r, i) in store.reputs" :key="i">
             <div class="rep-icon"></div><div class="rep-name">{{r.name}}</div>
-            <rate-parts :rate="r.rate"></rate-parts>
+            <rate-parts :rate="r.rate"/>
             <div class="repute">{{r.repute}}</div>
           </div>
         </div>
-        <img class="ad" src="img/NTTコミュニケーションズ.png"/>
+        <img class="ad" src="img/nttcom.png"/>
       </div>
     </div>
     <request-detail/>
@@ -43,30 +48,7 @@
 <script>
 import Request from './request.vue'
 import RequestDetail from './request-detail.vue'
-const Rate = {
-  template:[
-    '<div class="rating">',
-      '<div class="rate-base" :style="{width:rateWidth}"></div>',
-      '<div class="rate-cover">',
-        '<img class="star" src="img/star.png">',
-        '<img class="star" src="img/star.png">',
-        '<img class="star" src="img/star.png">',
-        '<img class="star" src="img/star.png">',
-        '<img class="star" src="img/star.png">',
-      '</div>',
-    '</div>'].join(''),
-  computed:{
-    rateWidth(){
-      const r = this.rate
-      const p = r / 5 * 100
-      return p + '%'
-    }
-  },
-  props:{
-    rate:Number
-  },
-}
-
+import Rate from './rate.vue'
 
 export default {
   data(){
